c85e31e142e614922e8d13a7bb9ba5c5476985b6
Andrey Gorokhov <fu1zee@gmail.com>
2020-05-01 20:47:02 +0300

    UI
    profileReducer
    gitignore

---
+++ ui/src/modules/users/reducers/profileReducer.ts
@@ -0,0 +1,44 @@
+import {SUCCESS} from 'Core/actions/actionTypes';
+import {EStatusCodes} from 'Core/reducer/enums';
+import {IAsyncData, IReduxAction} from 'Core/reducer/model';
+import {createAsyncDataReducer} from 'Core/reducer/utils';
+import {CLEAR_AUTH_DATA, LOGIN, SIGNUP} from 'Modules/auth/actions/actionTypes';
+import {
+    GET_USER_INFO,
+    TOGGLE_FAVOURITE,
+    UPDATE_ME,
+} from 'Modules/users/actions/actionTypes';
+import {IUserModel} from 'Modules/users/models';
+
+export const getInitialState = (): IAsyncData<IUserModel> => ({
+    status: EStatusCodes.IDLE,
+    data: null,
+    error: null,
+});
+
+const asyncActions = [LOGIN, SIGNUP, GET_USER_INFO, UPDATE_ME];
+
+export const profileReducer = (
+    state: IAsyncData<IUserModel> = getInitialState(),
+    action: IReduxAction<IUserModel>,
+) => {
+    if (asyncActions.includes(action.originalType)) {
+        return createAsyncDataReducer(action.originalType, state)(
+            state,
+            action,
+        );
+    }
+
+    if (action.type === TOGGLE_FAVOURITE + SUCCESS) {
+        return {
+            ...state,
+            data: action.payload.data,
+        };
+    }
+
+    if (action.type === CLEAR_AUTH_DATA) {
+        return getInitialState();
+    }
+
+    return state;
+};
